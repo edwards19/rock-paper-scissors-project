5594fab22dd7a58d63115e8976db6608b9fb4daf
Eduard <eduard.cojocaru26@gmail.com>
2022-09-02 19:49:48 +0300

    Change the if/else statements with switch, change winner()

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getComputerChoice() {
   if (Math.floor(Math.random()*100) <= 33) {
-    return 'Rock'
+    return 'rock'
   } else if (Math.floor(Math.random()*100) <= 66) {
-    return 'Paper'
+    return 'paper'
   } else if (Math.floor(Math.random()*100) <= 99) {
-    return 'Scissors'
+    return 'scissors'
   }
 };
 
@@ -14,47 +14,39 @@ let computerScore = 0;
 function playRound() {
   let playerSelection = prompt('Type either Rock, Paper or Scissors as your choice:', '').toLowerCase();
   let computerSelection = getComputerChoice();
-  if (playerSelection === '' || playerSelection === null) {
-    alert('You need to type either Rock, Paper of Scissors to play the game')
-  }
-  else if(playerSelection === 'rock' && computerSelection === 'Rock') {
-    return 'It\'s a tie!';
-  } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
-    computerScore++
-    return 'You lose! Paper beats Rock';
-  } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
-    playerScore++
-    return 'You win! Rock beats Scissors' 
-  } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
-    computerScore++
-    return 'You lose! Rock beats Scissors';
-  } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
-    playerScore++
-    return 'You win! Scissors beats Paper';
-  } else if (playerSelection === 'scissors' && computerSelection === 'Scissors') {
-    return 'It\'s a tie!';
-  } else if (playerSelection === 'paper' && computerSelection === 'Paper') {
-    return 'It\'s a tie!';
-  } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
-    playerScore++
-    return 'You win! Paper beats Rock';
-  } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
-    computerScore++
-    return 'You lose! Scissors beats Paper';
+  switch(true) {
+    case (playerSelection === '' || playerSelection === null):
+      alert('You need to type either Rock, Paper of Scissors to play the game')
+      break;
+    case (playerSelection === computerSelection):
+      return 'It\'s a tie!';
+    case (playerSelection === 'rock' && computerSelection === 'paper'):
+      computerScore++
+      return 'You lose! Paper beats Rock';
+    case (playerSelection === 'rock' && computerSelection === 'scissors'):
+      playerScore++
+      return 'You win! Rock beats Scissors'
+    case (playerSelection === 'scissors' && computerSelection === 'paper'):
+      playerScore++
+      return 'You win! Scissors beats Paper';
+    case (playerSelection === 'paper' && computerSelection === 'rock'):
+      playerScore++
+      return 'You win! Paper beats Rock';
+    case (playerSelection === 'paper' && computerSelection === 'scissors'):
+      computerScore++
+      return 'You lose! Scissors beats Paper';
   }
 };
-
 
 function winner(){
   if(playerScore > computerScore) {
-    console.log('You won! Congratulations')
+    console.log(`You won ${playerScore}-${computerScore}! Congratulations!`)
   } else if (playerScore < computerScore) {
-    console.log('You lost! Play again')
+    console.log(`You lost ${playerScore}-${computerScore}! Play again.`)
   } else if (playerScore === computerScore) {
-    console.log('It\'s a tie! Try again')
+    console.log(`It\'s a tie ${playerScore}-${computerScore}! Try again.`)
   }
 };
-
 
 function game() {
   let score = 5;
